e585c8ee42c65c48c5345e95eeb6d6424e39f529
frankjr12 <franklinjunior12@hotmail.com>
2023-09-11 11:12:07 -0300

    keep moving

--- main.py
+++ main.py
@@ -13,20 +13,25 @@ FACINGRIGHT = 1
 FACINGLEFT = -1
 DEFAULTGRAVITY = 1
 
+
 class Actor:
-    def __init__(self, xpos = 0, ypos = 0):
+    def __init__(self, xpos=0, ypos=0):
         self.pos = pygame.Vector2(xpos, ypos)
         self.image = None
-        self.body = pygame.Rect(self.pos.x, self.pos.y, DEFAULTRECTSIZE, DEFAULTRECTSIZE)
-        self.head = pygame.Rect(self.pos.x+DEFAULTRECTSIZE, self.pos.y, DEFAULTRECTSIZE/2, DEFAULTRECTSIZE/2)
+        self.body = pygame.Rect(self.pos.x, self.pos.y,
+                                DEFAULTRECTSIZE, DEFAULTRECTSIZE)
+        self.head = pygame.Rect(
+            self.pos.x+DEFAULTRECTSIZE, self.pos.y, DEFAULTRECTSIZE/2, DEFAULTRECTSIZE/2)
         self.bodycolor = "white"
         self.headcolor = "yellow"
         self.yspeed = 0
         self.facing = FACINGRIGHT
+
     def draw(self):
         pygame.draw.rect(screen, self.bodycolor, self.body)
         pygame.draw.rect(screen, self.headcolor, self.head)
-    def update(self, newx = 0, newy = 0):
+
+    def update(self, newx=0, newy=0):
         if newx != 0:
             if newx > self.pos.x:
                 self.facing = FACINGRIGHT
@@ -37,13 +42,13 @@ class Actor:
             if self.pos.x < 0:
                 self.pos.x = 0
             if self.pos.x > WIDTH:
-                self.pos.x = WIDTH        
+                self.pos.x = WIDTH
         self.pos.y = newy + self.yspeed
         # check for border
         if self.pos.y < 0:
             self.pos.y = 0
         if self.pos.y > HEIGHT:
-            self.pos.y = HEIGHT                
+            self.pos.y = HEIGHT
         self.body.x = self.pos.x
         self.body.y = self.pos.y
         if self.facing == FACINGRIGHT:
@@ -51,7 +56,8 @@ class Actor:
         else:
             self.head.x = self.pos.x-DEFAULTRECTSIZE/2
         self.head.y = self.pos.y
-## class Actor
+# class Actor
+
 
 class Surface:
     def __init__(self, xpos, ypos, width, height):
@@ -59,12 +65,16 @@ class Surface:
         self.image = None
         self.width = width
         self.height = height
-        self.body = pygame.Rect(self.pos.x, self.pos.y, self.width, self.height)
+        self.body = pygame.Rect(self.pos.x, self.pos.y,
+                                self.width, self.height)
         self.bodycolor = "green"
+
     def draw(self):
         pygame.draw.rect(screen, self.bodycolor, self.body)
+
     def update(self):
         pass
+
 
 # pygame setup
 pygame.init()
@@ -75,30 +85,45 @@ player = Actor(WIDTH / 2, HEIGHT / 2)
 floor = Surface(0, HEIGHT-HEIGHT*0.1, WIDTH, 4)
 surfaces = [floor]
 
+player_moving_left = False
+player_moving_right = False
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        #check if a key is pressed
+        # check if a key is pressed
         if event.type == pygame.KEYDOWN:
             # handles arrow keys
             if event.key == pygame.K_LEFT:
-                player.update(player.pos.x - DEFAULTSPEED, player.pos.y)
+                player_moving_left = True
+                #player.update(player.pos.x - DEFAULTSPEED, player.pos.y)
             if event.key == pygame.K_RIGHT:
-                player.update(player.pos.x + DEFAULTSPEED, player.pos.y)
+                player_moving_right = True
+                #player.update(player.pos.x + DEFAULTSPEED, player.pos.y)
             if event.key == pygame.K_UP:
                 player.update(player.pos.x, player.pos.y - DEFAULTSPEED)
             if event.key == pygame.K_DOWN:
-                player.update(player.pos.x, player.pos.y + DEFAULTSPEED)                
+                player.update(player.pos.x, player.pos.y + DEFAULTSPEED)
+        # check if key is still pressed
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                player_moving_left = False
+            if event.key == pygame.K_RIGHT:
+                player_moving_right = False
+    if player_moving_left:
+        player.update(player.pos.x - DEFAULTSPEED, player.pos.y)
+    if player_moving_right:
+        player.update(player.pos.x + DEFAULTSPEED, player.pos.y)
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
-    
-    # drawings 
+
+    # drawings
     for surface in surfaces:
-        surface.draw()    
+        surface.draw()
     player.draw()
 
     # logics
@@ -108,14 +133,12 @@ while running:
         if player.body.colliderect(surface.body):
             player.pos.y = surface.pos.y - DEFAULTRECTSIZE
             player.yspeed = 0
-        
-    
-    #updates
+
+    # updates
     player.update(player.pos.x, player.pos.y)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
-
 pygame.quit()
